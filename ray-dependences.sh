fd85b444f81bea4561401afff6247d4a6263635c
richardwlc <42689213+richardwlc@users.noreply.github.com>
2018-08-24 21:42:02 -0700

    Update ray-dependences.sh

--- ray-dependences.sh
+++ ray-dependences.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 sudo apt-get update
-sudo apt-get install python3-pip
+sudo apt-get install -y libxrender-dev libsm6 libxext6 zlib1g-dev libsnappy-dev python3-pip python3-dev
+sudo apt-get install -y git cmake pkg-config build-essential autoconf curl libtool unzip flex bison python
+pip3 install cmake cython tensorflow gym opencv-python scipy lz4
+pip3 install gym[atari]
 cd ~/.local/lib/python3.5/site-packages
 if [ -e ray ]
 then
@@ -9,3 +12,8 @@ else
     git clone https://github.com/ray-project/ray.git
     cd ray/
 fi
+git checkout d6af50785e2903d69c7809f019f7ea555f9f2688
+cd python
+pip3 install -e . --verbose
+
+echo "export OMP_NUM_THREADS=1" >> ~/.bashrc
